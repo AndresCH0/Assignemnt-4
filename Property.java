36b8aa332e38d85b2478ef148b21caac7b530073
AndresCH0 <98441234+AndresCH0@users.noreply.github.com>
2022-03-29 21:47:31 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -9,7 +9,13 @@ public class Property {
 	
 	//Constructors
 	//Default 
-	public Property() {}
+	public Property() {
+		propertyName = "";
+		city = "";
+		owner = "";
+		rentAmount = 0.0;
+		plot = new Plot();
+	}
 	
 	//Copy Constructor
 	public Property(Property p){
@@ -17,18 +23,20 @@ public class Property {
 		this.city = p.city;
 		this.owner = p.owner;
 		this.rentAmount = p.rentAmount;
-		this.plot = Property(p);
+		this.plot = new Plot(p.plot);
 	}
-	
-	//Pramaterized Property Constructor 
+
+
+	//Parameterized Property Constructor 
 	public Property(String propertyName, String city, double rentAmount, String owner){
 		this.propertyName = propertyName;
 		this.city = city;
 		this.rentAmount = rentAmount;
 		this.owner = owner;
+		this.plot = new Plot();
 	}
 	
-	//Pramaterized Property and Plot Constructor
+	//Parameterized Property and Plot Constructor
 	public Property(String propertyName, String city, double rentAmount, String owner, int x, int y, int width, int depth){
 		this.propertyName = propertyName;
 		this.city = city;
@@ -51,14 +59,14 @@ public class Property {
 	
 	
 	
-	//Sets the property's plot size	
+	//Setter and Getter for the property's plot	
 	public Plot setPlot(int x, int y, int width, int depth) {
-		this.plot = new Plot(x, y, width, depth);
-		
-		return plot;
+		return new Plot(x, y, width, depth);
 	}
 
-	
+	public Plot getPlot() {
+		return plot;
+	}
 	
 	
 	//Setter and getter for the property's city
@@ -97,7 +105,7 @@ public class Property {
 		return rentAmount;
 	}
 	
-	
+
 	//Prints out all the information about a property (city, name, owner, and the rent amount
 	public String toString() {
 		return "Property Name: " + propertyName + "\n" +
