00e45c1e6f4e98f5402fdb107d2b89068cf3ba19
AndresCH0 <98441234+AndresCH0@users.noreply.github.com>
2022-03-30 01:20:43 -0400

    Update ManagementCompanyTestSTUDENT.java

--- ManagementCompanyTestSTUDENT.java
+++ ManagementCompanyTestSTUDENT.java
@@ -8,39 +8,51 @@ import org.junit.Test;
 
 
 public class ManagementCompanyTestSTUDENT {
-	
+	Property p1 ,p2,p3,p4,p5,p6;
+	ManagementCompany m ;
 	@Before
 	public void setUp() throws Exception {
-		//student create a management company
+		p1 = new Property ("Hargerstown", "Rockville", 4844.00, "Sammy ScarFace",2,1,2,2);
+		p2 = new Property ("Lakehouse", "Lake", 4114, "Adam Taylor",4,1,2,2);
+		p3 = new Property ("Santiago Bernabeu", "Madrid", 4905, "Cristiano Ronaldo",6,1,2,2);
 		
-		//student add three properties, with plots, to mgmt co
+		m= new ManagementCompany("Railey", "555555555",6);
+	 
+		m.addProperty(p1);
+		m.addProperty(p2);
+		m.addProperty(p3);
 		
 	}
 
 	@After
 	public void tearDown() {
 		//student set mgmt co to null  
+		
+	p1=p2=p3=p4=p5=p6= null;
+	m = null;
 	}
 
 	@Test
 	public void testAddPropertyDefaultPlot() {
-		fail("STUDENT test not implemented yet");
 		//student should add property with 4 args & default plot (0,0,1,1)
+		p4 = new Property ("Home", "AdamsTown", 4000.0, "Me");
 		//student should add property with 8 args
+		p5 = new Property ("Montgomery College", "Rockville", 4905, "MC");
 		//student should add property that exceeds the size of the mgmt co array and can not be added, add property should return -1	
+		p6 = new Property ("Out Of Bounds", "NeverLand", 0.0, "FairyKing");
 	}
  
 	@Test
 	public void testMaxRentProp() {
-		fail("STUDENT test not implemented yet");
 		//student should test if maxRentProp contains the maximum rent of properties
+		assertEquals(m.maxRentProp(),4905,0);
 		
 	}
 
 	@Test
 	public void testTotalRent() {
-		fail("STUDENT test not implemented yet");
 		//student should test if totalRent returns the total rent of properties
+		assertEquals(m.totalRent(),8958.0,0);
 	}
 
  }
